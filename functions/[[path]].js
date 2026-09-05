a1fdfbc8ba10122ec0d79d0087242faa7c2c68e4
t-22-2022 <77286946+t-22-2022@users.noreply.github.com>
2026-09-05 09:52:33 +0800

    Update User-Agent version in headers

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -18,7 +18,7 @@ export async function onRequest(context) {
   console.log("🔗 Target:", target);
 
   const headers = new Headers();
-  headers.set("User-Agent", request.headers.get("User-Agent") || "OTT Navigator/1.9.6.4 (linux;android 10; en; 11gkvct)");
+  headers.set("User-Agent", request.headers.get("User-Agent") || "OTT Navigator/1.7.3.2 (linux;android 10; en; 11gkvct)");
   headers.set("Referer", target);
   headers.set("Origin", target);
 
